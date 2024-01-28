e48cb5d2bfc19de8abd561e9528daa1a073e8e9a
matt <russellmatt66@gmail.com>
2024-01-28 16:41:23 -0500

    working on Python tonnage project

---
+++ python/tonnage/tonnage.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+'''
+HELPER FUNCTIONS
+'''
+# WIP
+'''
+MAIN CODE
+'''
+path_to_lifting_numbers = sys.argv[1]
+
+lift_df = pd.read_csv(path_to_lifting_numbers)
+
+# WIP
