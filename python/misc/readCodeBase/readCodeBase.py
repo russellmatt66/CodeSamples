c94aed0904b4916c53838fc48cc0383b4ff460ad
Matt <russellmatt66@gmail.com>
2024-01-30 15:10:34 -0500

    DONE

--- python/misc/readCodeBase/readCodeBase.py
+++ python/misc/readCodeBase/readCodeBase.py
@@ -11,34 +11,42 @@ HELPER FUNCTIONS
 # Accepts a string representing the language to scan the code-base for
 # Returns a list of the file extensions to look for
 def readExtensions(prog_lang: str) -> list[str]:
+    print(prog_lang)
     with open('extensions.txt', 'r') as ext_file:
         for line in ext_file:
-            if line[0] == prog_lang:
-                # print("Found {}".format(prog_lang))
-                # extensions = line.split('=')[1]
-                # print(extensions)
-                pattern = re.compile("'(.+?)'")
+            if line.split('=')[0] == prog_lang:
+                pattern = re.compile("'.(.+?)'")
                 matches = pattern.findall(line)
-                # print(matches)
-                # if matches:
-                    # extensions = capture.group(1)
-                    # print(extensions)
+
     return matches
 
 # Reads a file, and returns how many lines there are 
-def readFile(path_to_file: str) -> int:
-    return {}
+def countLines(path_to_file: str) -> int:
+    l_c = 0
+    with open(path_to_file, 'r') as file:
+        for line in file:
+            l_c += 1
+    return l_c
 
 # Accepts a string representing the path to the codebase, an extension to scan for, and the dict in which to put the information 
-# Wrapper around the reading logic
+# Recursive wrapper around the reading logic
 def readCodeBase(root: str, ext_str: str, code_base_dict: dict) -> None:
     # Get a list of all relevant files, and sub-directories in root
     list_of_subdirs = next(os.walk(root))[1] # sub-directories
-    print(list_of_subdirs)
-    # list_of_files = [file for file in next(os.walk(root))[2] if file.split('.')[1] == ext_str] # files
-    list_of_files = next(os.walk(root))[2]
-    print(list_of_files)
-    # TODO Add error-handling to `list_of_files`
+    # print(list_of_subdirs)
+    list_of_files = [file for file in next(os.walk(root))[2] if (len(file.split('.')) != 1) and (file.split('.')[1] == ext_str)] # files
+
+    for file in list_of_files:
+        line_count = countLines(root + file)
+        code_base_dict['file_name'].append(file)
+        code_base_dict['path'].append(root) # This can be cleaned downstream
+        code_base_dict['line_count'].append(line_count)
+    
+    # print(code_base_dict)
+
+    for sub_dir in list_of_subdirs:
+        readCodeBase(root + sub_dir + "/", ext_str, code_base_dict)
+
     return
 
 '''
@@ -62,10 +70,11 @@ codebase_dict = {}
 features = ['file_name', 'path', 'line_count']
 for feature in features:
     codebase_dict[feature] = []
-    
+
 readCodeBase(path_to_code_base_root, ext_list[0], codebase_dict)
 # print(codebase_dict)
 
-# codebase_df = pd.DataFrame(codebase_dict)
+codebase_df = pd.DataFrame(codebase_dict)
+codebase_df.to_csv('./report_' + prog_lang + '.csv')
 # print(codebase_df)
 
--- python/misc/readCodeBase/test/testRCB.py
+++ python/misc/readCodeBase/test/testRCB.py
@@ -11,9 +11,10 @@ HELPER FUNCTIONS
 # Accepts a string representing the language to scan the code-base for
 # Returns a list of the file extensions to look for
 def readExtensions(prog_lang: str) -> list[str]:
+    print(prog_lang)
     with open('extensions.txt', 'r') as ext_file:
         for line in ext_file:
-            if line[0] == prog_lang:
+            if line.split('=')[0] == prog_lang:
                 # print("Found {}".format(prog_lang))
                 # extensions = line.split('=')[1]
                 # print(extensions)
@@ -26,25 +27,41 @@ def readExtensions(prog_lang: str) -> list[str]:
     return matches
 
 # Reads a file, and returns how many lines there are 
-def readFile(path_to_file: str) -> int:
-    return {}
+def countLines(path_to_file: str) -> int:
+    l_c = 0
+    with open(path_to_file, 'r') as file:
+        for line in file:
+            l_c += 1
+    return l_c
 
 # Accepts a string representing the path to the codebase, an extension to scan for, and the dict in which to put the information 
-# Wrapper around the reading logic
+# Recursive wrapper around the reading logic
 def readCodeBase(root: str, ext_str: str, code_base_dict: dict) -> None:
     # Get a list of all relevant files, and sub-directories in root
     list_of_subdirs = next(os.walk(root))[1] # sub-directories
     print(list_of_subdirs)
-    # list_of_files = [file for file in next(os.walk(root))[2] if file.split('.')[1] == ext_str] # files
-    list_of_files = next(os.walk(root))[2]
+    list_of_files = [file for file in next(os.walk(root))[2] if (len(file.split('.')) != 1) and (file.split('.')[1] == ext_str)] # files
+    # list_of_files = next(os.walk(root))[2]
     print(list_of_files)
+
     for file in list_of_files:
-        dot_split = file.split('.')
-        if len(dot_split) == 1:
-            continue
-        print(file)
-        print(file.split('.'))
-        print(file.split('.')[0], file.split('.')[1])
+        line_count = countLines(root + file)
+        code_base_dict['file_name'].append(file)
+        code_base_dict['path'].append(root) # This can be cleaned downstream
+        code_base_dict['line_count'].append(line_count)
+    
+    print(code_base_dict)
+
+    for sub_dir in list_of_subdirs:
+        readCodeBase(root + sub_dir + "/", ext_str, code_base_dict)
+
+    # for file in list_of_files:
+    #     dot_split = file.split('.')
+    #     if len(dot_split) == 1:
+    #         continue
+    #     print(file)
+    #     print(file.split('.'))
+    #     print(file.split('.')[0], file.split('.')[1])
         # file.delete()
     # TODO Add error-handling to `list_of_files`
     return
@@ -71,9 +88,9 @@ features = ['file_name', 'path', 'line_count']
 for feature in features:
     codebase_dict[feature] = []
 
-print(codebase_dict)
-
-codebase_df = pd.DataFrame(codebase_dict)
-print(codebase_df)
-
 readCodeBase(path_to_code_base_root, ext_list[0], codebase_dict)
+# print(codebase_dict)
+
+# codebase_df = pd.DataFrame(codebase_dict)
+# print(codebase_df)
+
