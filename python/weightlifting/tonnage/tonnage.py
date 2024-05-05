fec6a364e80588906cd4e1c576fa3d544e094d6d
Matt <russellmatt66@gmail.com>
2024-05-05 00:30:37 -0400

    More work on tonnage

--- python/weightlifting/tonnage/tonnage.py
+++ python/weightlifting/tonnage/tonnage.py
@@ -1,15 +1,42 @@
 import pandas as pd
+import re
 
-def anyBackoffSets(exercise_instance: pd.Series) -> bool, int:
-    where = 0
+# Maximum of five working sets
+def anyBackoffSets(exercise_instance: pd.Series) -> bool, list[int]:
+    where = []
+    idx = 0
+    backoff_truth = False
     for labels, weight_or_reps in exercise_instance.items():
         if ':' in weight_or_reps:
-            return True, where
-        where += 1
+            backoff_truth = True 
+            where.append(idx)
+        idx += 1
 
-    where = -1 # safe failure code
-    return False, where
+    return backoff_truth, where
 
-def anyEquipment(exercise_instance: pd.Series) -> bool, list[str]:
-    # Check if the weight has 'KS', 'ES', 'WS', or some composite attached
-        
+def parseWorkingWeight(string):
+    # Match numeric characters
+    numeric_part = re.match(r'\d+', string).group(0)
+    # Match non-numeric characters
+    nonnumeric_part = re.sub(r'\d+', '', string)
+
+    return numeric_part, nonnumeric_part
+
+def anyEquipment(exercise_instance: pd.Series) -> bool, list[int] list[str]:
+    # Check if the working weight has 'KS', 'ES', 'WS', or some composite form, attached which indicates the usage of sleeves, wrist straps/wraps, or a combination
+    # Also checks if equipment was used during backoff sets
+    equipment_truth = False
+    equipment_kinds = []
+    
+    working_weight_string = exercise_instance.loc[exercise_instance['WEIGHT (LBS)']]
+    
+    # Use regex to seperate out the equipment kinds 
+    working_weight, equipment_kinds = parseWorkingWeight(working_weight_string)
+
+    any_backoff_sets, bidcs = anyBackoffSets(exercise_instance)
+    for bidx in bidcs:
+        backoff_string = exercise_instance.iloc[bidx]
+
+        # Use string methods to seperate out backoff weight from string
+
+        backoff_weight = 
