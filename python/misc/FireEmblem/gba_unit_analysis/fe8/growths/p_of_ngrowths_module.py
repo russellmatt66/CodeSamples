7713da0c9ddfe56c44b6839d71db82215c8f8b40
matt <russellmatt66@gmail.com>
2024-05-13 17:18:07 -0400

    working on module

--- python/misc/FireEmblem/gba_unit_analysis/fe8/growths/p_of_ngrowths_module.py
+++ python/misc/FireEmblem/gba_unit_analysis/fe8/growths/p_of_ngrowths_module.py
@@ -5,7 +5,7 @@ def NumberOfMicrostates(n) -> int:
     # Just 7 choose n
     # 7 is the total number of stats
     # n is the number that increased upon a level up
-    return np.factorial(7) / (np.factorial(n) * np.factorial(7 - n))
+    return int(np.math.factorial(7) / (np.math.factorial(n) * np.math.factorial(7 - n)))
 
 def GrowthProduct(unit_growths: pd.Series, G: list[int]) -> np.float64:
     # G is the list of indices representing growths which did not increase
@@ -17,13 +17,27 @@ def GrowthProduct(unit_growths: pd.Series, G: list[int]) -> np.float64:
 
     return gp
 
+def CreateGs(n: int, Nm: int) -> list[list[int]]:
+    # list of the G's for a given n
+    list_of_Gs = []
+
+    n_did_not_increase = 7 - n # 7 is total number of stats
+
+    # need to create all tuples representing the combinations of stats which did not increase
+    temp_G = list(range(n_did_not_increase))
+    print(temp_G)
+
+    return list_of_Gs
+
 def NGrowthProbability(n: int, unit_growths: pd.Series) -> np.float64:
     P_of_n = 0.0
 
     Nm = NumberOfMicrostates(n)
+    list_of_Gs = CreateGs(Nm)
     i = 0
     while i < Nm:
-        G = GetG()
+        G = list_of_Gs[i]
         P_of_n += GrowthProduct(unit_growths, G)
+        i += 1
 
     return P_of_n
